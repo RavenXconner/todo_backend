61a3dbf3087db65c488ee27bd752c9684378dd31
Javy Rodillon <reyfoxconner@gmail.com>
2025-04-10 21:04:52 +0800

    add token authentication and enforce permissions for Todo API

--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Todo
 from .serializers import TodoSerializer
 
 class TodoViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]  
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
