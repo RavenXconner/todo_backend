2f892d5d56b36751d246147c34ee9458d7b562e8
Javy Rodillon <reyfoxconner@gmail.com>
2025-04-13 17:50:39 +0800

    add CORS_ALLOWED_ORIGINS to settings for local and production environments

--- todo_api/settings.py
+++ todo_api/settings.py
@@ -146,3 +146,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173", # Allow React Vite app locally
+    "https://rodillon-todolist.netlify.app/",  # Allow Netlify app  # Allow React Vite app
+]
